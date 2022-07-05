8c3371ada4bec260d9cbf484562a893f77eb51d7
adamah <adama.diakhate1@uvs.edu.sn>
2022-07-05 13:16:46 +0000

    mis a jour

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,10 +7,9 @@ import 'page-http-code/dist/index.css'
 
 const App = () => {
     var text = ''
-    var typePage = 404
     var img = ''
     var redirectLink = 'https://volkeno.sn'
-  return <HttpCodePage text={text} image={img} typePage={typePage} redirectLink={redirectLink} />
+  return <HttpCodePage text={text} image={img} typePage={404} redirectLink={redirectLink} />
 }
 
 export default App
